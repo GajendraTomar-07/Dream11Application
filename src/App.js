bb0ff978d046458494a63cae25d9e3cc9679c8f4
GajendraTomar-07 <148423802+GajendraTomar-07@users.noreply.github.com>
2024-12-20 17:56:21 +0530

    Commited HeroSection

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import Header from './components/Header';
 import HeroSection from './components/HeroSection';
+import FaqSection from './components/FaqSection';
 
 function App() {
   return (
    
     <>
     <Header/>
-    {/* <HeroSection/> */}
+    <HeroSection/>
+    <FaqSection/>
+    
     </>
   );
 }
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,14 +1,122 @@
+import React from "react";
+import { Grid, Box } from "@mui/material";
+import Destop2024 from "../assets/Destop2024.webp";
+import Desktop1 from "../assets/Desktop1.webp";
+import Desktop2 from "../assets/Desktop2.webp";
+import Desktop3 from "../assets/Desktop3.webp";
+import Desktop4 from "../assets/desktop4.webp"
+import Slider from "react-slick";
+import "./HeroSection.css";
+import { useState, useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
-import React from 'react'
 
 const HeroSection = () => {
-  return (
-  <>
-  
-  hero 
-  </>
-  )
-}
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
 
-export default HeroSection
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      // once: true,
+    });
+
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth <= 767);
+    };
+
+    handleResize(); // Initial check
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
+  return (
+     <Grid
+       sx={{
+        width: "100%",
+        height: "100vh",
+        backgroundImage: `url(${Destop2024})`,
+        // backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+          '@media (max-width: 600px)': {
+          backgroundSize: 'auto',
+          backgroundPosition: 'center top',
+        },
+        '@media (max-width: 960px)': {
+          backgroundSize: 'cover',
+        },
+        "@media (max-width: 480px)": {
+          backgroundImage: "none",
+          backgroundColor: "#050c1f", 
+        },
+      }}
+     >
+      <Grid item>
+    <Box  >
+      <Slider {...settings} >
+        <div  
+        data-aos="zoom-in"
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <img
+            src={isSmallScreen ? Desktop4 : Desktop1}
+            alt="desktop"
+            style={{ width: "50%", height: "85vh",margin:"auto",  marginTop: "80px", }}
+          />
+        </div>
+        <div 
+          data-aos="zoom-in"
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <img
+            src={Desktop2}
+            alt="desktop"
+            style={{ width: "50%", height: "85vh",margin:"auto",  marginTop: "80px", }}
+          />
+        </div>
+        <div  
+         data-aos="zoom-in"
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <img
+            src={Desktop3}
+            alt="desktop"
+            style={{ width: "50%", height: "85vh",margin:"auto",  marginTop: "80px",}}
+          />
+        </div>
+       
+      </Slider>
+    </Box>
+      </Grid>
+     </Grid>
+  );
+};
+
+export default HeroSection;
