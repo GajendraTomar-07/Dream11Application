8c020c916a80b03b46533b312c1c61f367a29fac
Yash Bagora <ydbagora@gmail.com>
2024-12-23 11:34:17 +0530

    Commiting App.js file and App.css

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import HeroSection from './components/HeroSection';
+import OfficialPartner from './components/OfficialPartner';
+import PlayFeature from './components/PlayFeature';
+import FantasyPage from './components/FantasyPage';
+import FooterPage from './components/FooterPage';
 
 function App() {
   return (
@@ -9,6 +13,10 @@ function App() {
     <>
     <Header/>
     {/* <HeroSection/> */}
+    <OfficialPartner/>
+    <PlayFeature/>
+    <FantasyPage/>
+    <FooterPage/>
     </>
   );
 }
